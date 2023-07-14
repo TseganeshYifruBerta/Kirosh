e2c619e8521b4fd89b3817632ebf57fc2488cc50
Tseganesh Yifru <tseganeshyifruberta>
2023-07-14 22:59:13 +0300

    landing page responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Nav from '@/components/layout/Nav'
 import Footer from '@/components/layout/Footer'
+import LandingPage from '@/components/home/landing'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +10,7 @@ export default function Home() {
   
   return (
     <div className='h-screen'>
+      <LandingPage />
     </div>
   )
 }
